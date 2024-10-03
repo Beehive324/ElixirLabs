ba15224a53cdb159fcb66abd7ae6a51b95821fb9
Viery <63168364+Beehive324@users.noreply.github.com>
2024-10-03 16:09:31 +0100

    Create power.ex

---
+++ power.ex
@@ -0,0 +1,31 @@
+defmodule Powers do
+    def square(n) do
+        n * n
+    end
+    
+end
+
+
+defmodule Division do
+    def divised(a, b) do
+        a / b
+
+    end
+end
+
+
+defmodule Cube do
+    def cube(x) do
+        Powers.square(x) * x
+    end
+end
+
+
+defmodule MultiClause do
+    def square_or_cube
+
+
+
+
+
+
